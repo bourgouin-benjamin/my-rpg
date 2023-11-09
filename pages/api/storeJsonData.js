81e94f13287451d7b65a8e7f99ec03b8be6aa183
Benjamin Bourgouin <benjamin.bourgouin.pro@gmail.com>
2023-11-08 21:30:23 -0500

    Lecture et ecriture d'un fichier JSON via une API

---
+++ pages/api/storeJsonData.js
@@ -0,0 +1,35 @@
+import fsPromises from 'fs/promises';
+import path from 'path';
+
+const DATA_FILE_PATH = path.join(process.cwd(), 'json/save.json');
+
+export default async function handler(req, res) {
+	if (req.method === 'GET') {
+		const jsonData = await fsPromises.readFile(DATA_FILE_PATH);
+		const objectData = JSON.parse(jsonData);
+
+		res.status(200).json(objectData);
+	} else if (req.method === 'POST') {
+		try {
+			const jsonData = await fsPromises.readFile(DATA_FILE_PATH);
+			const objectData = JSON.parse(jsonData);
+
+			const { name, email } = req.body;
+
+			const newData = {
+				name,
+				email,
+			};
+			objectData.push(newData);
+
+			const updatedData = JSON.stringify(objectData);
+
+			await fsPromises.writeFile(DATA_FILE_PATH, updatedData);
+
+			res.status(200).json({ message: 'Data stored successfully' });
+		} catch (error) {
+			console.error(error);
+			res.status(500).json({ message: 'Error storing data' });
+		}
+	}
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,46 @@
+// Librairie
+import { useRouter } from 'next/router';
+
 function Home() {
+	// Variable
+	const router = useRouter();
+
+	// Méthode
+	const fetchData = async () => {
+		const response = await fetch('/api/storeJsonData');
+		const data = await response.json();
+		console.log('data', data);
+	};
+
+	const saveData = async () => {
+		const response = await fetch('/api/storeJsonData', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				name: 'pepito',
+				email: 'pepito@pepito.com',
+			}),
+		});
+		const data = await response.json();
+		console.log(data);
+	};
+
+	const onNameSubmit = (e) => {
+		e.preventDefault();
+	};
+
 	return (
 		<main>
-			<h1>Hello World !</h1>
+			<h1>Quel est votre nom, aventurier ?</h1>
+			<form onSubmit={onNameSubmit}>
+				<input type="text" name="name" id="name" />
+				<button type="submit" onClick={fetchData}>
+					Valider
+				</button>
+			</form>
+			<button onClick={saveData}>Test ajout datas</button>
 		</main>
 	);
 }
